9c173d8c598892968f5cf9ef68d835b050fc085c
Ayushmadhur5588 <cuteboy.bhardwaj@gmail.com>
2025-03-05 10:44:56 +0530

    Update

--- 02-course-app-easy/index.js
+++ 02-course-app-easy/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.json());
@@ -9,87 +9,85 @@ let COURSES = [];
 let id = 1;
 
 const authenticateAdmin = (req, res, next) => {
-const {username, password} = req.headers;
-const adminExist = ADMINS.find((a) => a.username === username && a.password ===  password);
-if(adminExist){
-  next();
-}else{
-  res.status(401).json({message : "Admin does Exists"});
-}
-}
+  const { username, password } = req.headers;
+  const adminExist = ADMINS.find(
+    (a) => a.username === username && a.password === password
+  );
+  if (adminExist) {
+    next();
+  } else {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+};
 
 // Admin routes
-app.post('/admin/signup', (req, res) => {
-  const {username, password} = req.headers;
+app.post("/admin/signup", (req, res) => {
+  const { username, password } = req.headers;
   const existingAdmin = ADMINS.find((a) => a.username === username);
-  if(existingAdmin){
-    res.status(409).json({message : "Admin already exists"});
-  }else{
-    ADMINS.push({username, password});
-    res.status(201).json({ message: 'Admin created successfully'});
+  if (existingAdmin) {
+    res.status(409).json({ message: "Admin already exists" });
+  } else {
+    ADMINS.push({ username, password });
+    res.status(201).json({ message: "Admin created successfully" });
   }
 });
 
-app.post('/admin/login', authenticateAdmin, (req, res) => {
-  res.json({ message: 'Logged in successfully' });
+app.post("/admin/login", authenticateAdmin, (req, res) => {
+  res.json({ message: "Logged in successfully" });
 });
 
-app.post('/admin/courses', authenticateAdmin, (req, res) => {
-  const { title, desc, price, imagelink } = req.body;
+app.post("/admin/courses", authenticateAdmin, (req, res) => {
+  const { title, description, price, imageLink } = req.body;
 
   const course = {
-    id: ++id, 
+    id: ++id,
     title,
-    desc,
+    description,
     price,
-    imagelink
+    imageLink,
+    published: false,
   };
 
   COURSES.push(course);
-  res.json({ message: 'Course created successfully', courseId: id });
+  res.json({ message: "Course created successfully", courseId: id });
 });
 
-app.put('/admin/courses/:courseId',authenticateAdmin,  (req, res) => {
+app.put("/admin/courses/:courseId", authenticateAdmin, (req, res) => {
   const courseId = parseInt(req.params.courseId);
-  const idx = COURSES.findIndex((c) => c.id === courseId);
-  if(idx !== -1){
-    const { title, description, price, imageLink, published } = req.body;
-   if(title !== undefined) COURSES[idx].title = title;
-   if(description !== undefined) COURSES[idx].description = description;
-   if(price !== undefined) COURSES[idx].price = price;
-   if(imageLink !== undefined) COURSES[idx].imageLink = imageLink;
-   if(published !== undefined) COURSES[idx].published = published;
-    res.json({ message: 'Course updated successfully' });
-  }else{
-    res.status(404).json({message : "Course doesn't exist"});
+  const course = COURSES.find((c) => c.id === courseId);
+  if (course) {
+    Object.assign(course, req.body);
+    res.json({ message: "Course updated successfully" });
+  } else {
+    res.status(404).json({ message: "Course doesn't exist" });
   }
 });
 
-app.get('/admin/courses', authenticateAdmin, (req, res) => {
-  res.json({ courses: COURSES});
+app.get("/admin/courses", authenticateAdmin, (req, res) => {
+  res.json({ courses: COURSES });
 });
 
 // User routes
-app.post('/users/signup', (req, res) => {
+app.post("/users/signup", (req, res) => {
   // logic to sign up user
 });
 
-app.post('/users/login', (req, res) => {
+app.post("/users/login", (req, res) => {
   // logic to log in user
 });
 
-app.get('/users/courses', (req, res) => {
+app.get("/users/courses", (req, res) => {
   // logic to list all courses
 });
 
-app.post('/users/courses/:courseId', (req, res) => {
+app.post("/users/courses/:courseId", (req, res) => {
   // logic to purchase a course
 });
 
-app.get('/users/purchasedCourses', (req, res) => {
+app.get("/users/purchasedCourses", (req, res) => {
   // logic to view purchased courses
 });
 
 app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+  console.log("Server is listening on port 3000");
 });
